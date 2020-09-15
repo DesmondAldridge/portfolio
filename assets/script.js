77a723fb5874756ea736ed529016cf73eba30b04
Desmond <desmondaldridge@gmail.com>
2020-09-15 02:44:25 -0700

    Styling in the modals

--- assets/script.js
+++ assets/script.js
@@ -10,25 +10,8 @@ function openTab(evt, tabName) {
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
-    tablinks[i].classList.remove("w3-light-grey");
+    tablinks[i].classList.remove("currentTab");
   }
   document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.classList.add("w3-light-grey");
-}
-
-//'Portfolio' Modal
-document.getElementsByClassName("tablink")[0].click();
-
-function openTab(evt, tabName) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("aboutMeTab");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < x.length; i++) {
-    tablinks[i].classList.remove("w3-light-grey");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.classList.add("w3-light-grey");
+  evt.currentTarget.classList.add("currentTab");
 }
